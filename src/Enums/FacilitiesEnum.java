fda7fd289dd61be173d9ae704620f2573eeb1ba4
Anıl Karaşah <anilkarasah@hotmail.com>
2023-01-04 20:34:56 +0300

    Update FacilitiesEnum.java

--- src/Enums/FacilitiesEnum.java
+++ src/Enums/FacilitiesEnum.java
@@ -1,21 +1,19 @@
 package Enums;
 
-public class FacilitiesEnum {
-    public static final String[] List;
+import java.util.ArrayList;
 
-    static {
-        List = new String[] {
-                "KidsAreGuest",
-                "PetsAreAllowed",
-                "HasWifi",
-                "HasCarParking",
-                "HasSpa",
-                "HasGym",
-                "HasRestaurant",
-                "HasRoomService",
-                "HasPool"
-        };
-    }
+public class FacilitiesEnum {
+    public static final String[] List = new String[] {
+	        "Çocuk Misafir",
+	        "Evcil Hayvan Girebilir",
+	        "Kablosuz İnternet",
+	        "Otopark",
+	        "Spa / Hamam",
+	        "Spor Salonu",
+	        "Restoran",
+	        "Oda Servisi",
+	        "Açık / Kapalı Havuz"
+	    };
 
     public static Integer getFacilityIndex(String facility) {
         for (int i = 0; i < List.length; i++) {
@@ -28,5 +26,14 @@ public class FacilitiesEnum {
 
     public static String getFacilityName(int index) {
         return List[index];
+    }
+    
+    public static ArrayList<String> getFacilitiesList(boolean[] facilityFlags) {
+    	ArrayList<String> result = new ArrayList<>();
+    	
+    	for (int i = 0; i < facilityFlags.length; i++)
+    		if (facilityFlags[i]) result.add(List[i]);
+    	
+    	return result;
     }
 }
