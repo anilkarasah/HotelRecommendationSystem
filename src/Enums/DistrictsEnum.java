2cadabda4d103aed4d5d4839eff0e13e8644dce7
Anıl Karaşah <anilkarasah@hotmail.com>
2023-01-04 16:15:51 +0300

    Districts Enum now available

--- src/Enums/DistrictsEnum.java
+++ src/Enums/DistrictsEnum.java
@@ -1,9 +1,29 @@
 package Enums;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class DistrictsEnum {
-	public static HashMap<String, String[]> CityMap = new HashMap<>() {
+	public static HashMap<String, ArrayList<String>> cityMap;
+	
+	public static void setDistrictsEnum(HashMap<String, ArrayList<String>> cityDistrictMap) {
+		cityMap = cityDistrictMap;
+	}
+	
+	public static ArrayList<String> addCity(String cityName) {
+		return cityMap.put(cityName, null);
+	}
+	
+	public static boolean addDistrictsToCity(String cityName, ArrayList<String> districtList) {
+		ArrayList<String> currentDistrictList = cityMap.get(cityName);
+		if (currentDistrictList == null) {
+			currentDistrictList = addCity(cityName);
+		}
 		
-	};
+		return currentDistrictList.addAll(districtList);
+	}
+	
+	public static ArrayList<String> getDistrictsOfCity(String cityName) {
+		return cityMap.get(cityName);
+	}
 }
--- src/Services/CSVService.java
+++ src/Services/CSVService.java
@@ -2,6 +2,9 @@ package Services;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import Enums.DistrictsEnum;
 import Models.Hotel;
 
 public class CSVService {
@@ -18,11 +21,23 @@ public class CSVService {
         ArrayList<Hotel> hotels = new ArrayList<>();
 
         line = reader.readLine();
+        HashMap<String, ArrayList<String>> cityMap = new HashMap<String, ArrayList<String>>();
 
         while ((line = reader.readLine()) != null) {
             Hotel hotel = Hotel.parseCSV(line);
+
+            ArrayList<String> districtList;
+            if ((districtList = cityMap.get(hotel.province)) != null) {
+            	if (!districtList.contains(hotel.district))
+            		districtList.add(hotel.district);
+            } else {
+            	cityMap.put(hotel.province, new ArrayList<String>());
+            }
+
             hotels.add(hotel);
         }
+        
+        DistrictsEnum.setDistrictsEnum(cityMap);
 
         reader.close();
 
